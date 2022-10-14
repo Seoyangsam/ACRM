01eeb087522840798d150f9ae34124f3bb78d5eb
mawang <max.wang@ugent.be>
2022-10-14 11:01:05 +0200

    test

---
+++ test/test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import os
+
+# load in data
+facilities = pd.read_csv("../data/facilities.csv")
+incidents = pd.read_csv("../data/incidents.csv")
+satisfaction = pd.read_csv("../data/satisfaction.csv")
+stations = pd.read_csv("../data/stations.csv")
+stops = pd.read_csv("../data/stops.csv")
+
+
+
+
