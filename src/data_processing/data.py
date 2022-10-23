9e651e165c7df8e096a8b33742c95170282298cc
annelienfeys <annelien.feys@Ugent.be>
2022-10-23 10:09:09 +0200

    Vraag 3

--- src/data_processing/data.py
+++ src/data_processing/data.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from pandas.api.types import is_datetime64_any_dtype as is_datetime
 import src.utils as utils
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.core.pylabtools import figsize
 
 
 #########################################################   LOAD IN DATA  ##########################################################
@@ -150,3 +153,22 @@ def get_delay(expected, real):
     return delay
 
 full_trips['Delay time'] = full_trips.apply(lambda obs: get_delay(obs['Time of planned arrival'], obs['Time of real arrival']), axis = 1)
+
+
+#vraag3: Plot the number of stations per city for cities with multiple stations.
+
+geg = pd.read_csv(r'C:\users\Annelien\Documents\SCHOOL\ACRM\Project NMBS\group7-group-assignment-\Data\facilities.csv', sep=',')
+df = pd.DataFrame(geg)
+df2 = df.groupby('city').size().reset_index(name='count') # head(5)dan toon je maar 5lijnen, eruit halen op termijn
+#groepeer per city en geef de lengte hier mee
+#resetindex = maak nieuwe kolom aan en noem die count
+# df2 = count > 1
+print(df2)
+df2 = df2[df2["count"] > 1 ]
+
+plt.bar(df2['city'], df2['count'], color='r')
+plt.xlabel("city")
+plt.ylabel("number of stations")
+plt.legend(loc="upper left")
+plt.xticks(rotation= "vertical")
+plt.show()
